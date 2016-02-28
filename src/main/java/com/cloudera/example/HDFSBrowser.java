36f6672f25be9a616e64d3656712b4b1299d16c7
Ashok Rajan <arajan@cloudera.com>
2016-02-28 13:49:53 -0700

    Initial commit

---
+++ src/main/java/com/cloudera/example/HDFSBrowser.java
@@ -0,0 +1,57 @@
+package com.cloudera.example;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+
+/**
+ * Created by Ashok Rajan.
+ */
+public class HDFSBrowser {
+
+    public void listFolder(String folderPath) throws IOException {
+        Configuration conf = new Configuration();
+
+        // Conf object will read the HDFS configuration parameters from these
+        // XML files.
+        conf.addResource(new Path("/etc/hadoop/conf/core-site.xml"));
+        conf.addResource(new Path("/etc/hadoop/conf/hdfs-site.xml"));
+        Path path = new Path(folderPath);
+        FileSystem fileSystem = FileSystem.get(conf);
+        FileStatus[] files = fileSystem.globStatus(path);
+        for (FileStatus file : files ){
+            System.out.println(file.getPath().getName());
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length < 1) {
+            System.out.println("Usage: HDFSBrowser list <hdfs_path>" +
+                    " [<local_path> <hdfs_path>]");
+            System.exit(1);
+        }
+
+        HDFSBrowser browser = new HDFSBrowser();
+        if (args[0].equals("list")) {
+            if (args.length < 2) {
+                System.out.println("Usage: HDFSBrowser list <hdfs_path>");
+                System.exit(1);
+            }
+
+            browser.listFolder(args[1]);
+        }
+
+        System.out.println("Done!");
+    }
+
+}
