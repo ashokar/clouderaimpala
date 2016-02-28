5bf952614b3bf44325e568a97a02f692d1360c65
Ashok Rajan <arajan@cloudera.com>
2016-02-28 13:57:48 -0700

    HDFS Browser

--- src/main/java/com/cloudera/example/HDFSBrowser.java
+++ src/main/java/com/cloudera/example/HDFSBrowser.java
@@ -18,6 +18,8 @@ import org.apache.hadoop.fs.*;
 public class HDFSBrowser {
 
     public void listFolder(String folderPath) throws IOException {
+
+        System.out.println("Folder to list "+folderPath);
         Configuration conf = new Configuration();
 
         // Conf object will read the HDFS configuration parameters from these
@@ -27,6 +29,7 @@ public class HDFSBrowser {
         Path path = new Path(folderPath);
         FileSystem fileSystem = FileSystem.get(conf);
         FileStatus[] files = fileSystem.globStatus(path);
+        System.out.println("Listing "+folderPath);
         for (FileStatus file : files ){
             System.out.println(file.getPath().getName());
         }
@@ -36,17 +39,18 @@ public class HDFSBrowser {
     public static void main(String[] args) throws IOException {
 
         if (args.length < 1) {
-            System.out.println("Usage: HDFSBrowser list <hdfs_path>" +
-                    " [<local_path> <hdfs_path>]");
+            System.out.println("Usage: HDFSBrowser list <hdfs_path>");
             System.exit(1);
         }
 
         HDFSBrowser browser = new HDFSBrowser();
+
         if (args[0].equals("list")) {
             if (args.length < 2) {
                 System.out.println("Usage: HDFSBrowser list <hdfs_path>");
                 System.exit(1);
             }
+            System.out.println("browser.listFolder "+args[1]);
 
             browser.listFolder(args[1]);
         }
